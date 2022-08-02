144f4f3ad0839221645de28eb318865782ddc84e
Alhassan Mohamed <86925987+Mhmh-M@users.noreply.github.com>
2022-08-03 01:43:09 +0200

    fix some pugs and update sections

---
+++ main.js
@@ -0,0 +1,83 @@
+let links_ul = document.querySelector("#links");
+let toggle_menu = document.querySelector("#toggle_menu");
+let spans = document.querySelector("#toggle_menu span");
+let menu_active = false;
+let center = document.querySelector("#btn");
+let social = document.querySelectorAll('.container-contact .social');
+let whatsApp = document.querySelector("#whatsApp");
+
+
+
+toggle_menu.addEventListener('click', function (e) {
+  e.stopPropagation;
+  if (document.querySelector("html").style.width < 767 + "px") {
+    if (!menu_active) {
+      menu_active = true
+      toggle_menu.classList.toggle("active-icon");
+      links_ul.style.display = "block";
+    } else {
+      menu_active = false
+      toggle_menu.classList.remove("active-icon");
+      links_ul.style.display = "none";
+    }
+  } else {
+    links_ul.style.display = "flex"
+  }
+});
+
+links_ul.onclick = function (e) {
+  e.stopPropagation;
+}
+
+spans.onclick = function (e) {
+  e.stopPropagation
+}
+
+center.addEventListener('click', () => {
+  social.forEach((e) => {
+    e.classList.toggle('rotate')
+  });
+  console.log("Hello From Test")
+
+}
+)
+
+whatsApp.addEventListener('click', (e) => {
+  let overlay = document.createElement('div');
+
+  overlay.className = 'popup-overlay';
+
+  document.body.appendChild(overlay);
+
+  let popupBox = document.createElement('div');
+
+  popupBox.className = 'popup-box';
+
+  document.body.appendChild(popupBox)
+  let whatsNumber = document.createElement("h3");
+
+  let numberText = document.createTextNode("+201008202048");
+
+  whatsNumber.appendChild(numberText);
+
+  popupBox.appendChild(whatsNumber);
+
+  let closeButton = document.createElement("span");
+
+  let closeButtonText = document.createTextNode("X");
+
+  closeButton.appendChild(closeButtonText);
+
+  popupBox.appendChild(closeButton);
+
+  closeButton.className = 'close-botton';
+})
+
+document.addEventListener('click', function (e) {
+  if (e.target.className == 'close-botton') {
+
+    e.target.parentNode.remove();
+
+    document.querySelector(".popup-overlay").remove();
+  }
+})
