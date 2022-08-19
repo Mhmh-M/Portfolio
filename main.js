e1ff4297a15fe543bef014bdc349f3b2d88ecb23
muhammed <z01100230021@gmail.com>
2022-08-19 22:32:53 +0200

    add new project and update contact section

--- main.js
+++ main.js
@@ -14,20 +14,18 @@ let spans_Skills = document.querySelectorAll(".the-progress span");
 
 toggle_menu.addEventListener('click', function (e) {
   e.stopPropagation;
-  if (document.querySelector("html").style.width < 767 + "px") {
-    if (!menu_active) {
-      menu_active = true
-      toggle_menu.classList.toggle("active-icon");
-      links_ul.style.display = "block";
-    } else {
-      menu_active = false
-      toggle_menu.classList.remove("active-icon");
-      links_ul.style.display = "none";
-    }
+  if (!menu_active) {
+    menu_active = true
+    toggle_menu.classList.toggle("active-icon");
+    links_ul.style.display = "block";
   } else {
-    links_ul.style.display = "flex"
+    menu_active = false
+    toggle_menu.classList.remove("active-icon");
+    links_ul.style.display = "none";
   }
 });
+
+
 
 links_ul.onclick = function (e) {
   e.stopPropagation;
@@ -37,44 +35,12 @@ spans.onclick = function (e) {
   e.stopPropagation
 }
 
-center.addEventListener('click', () => {
-  social.forEach((e) => {
-    e.classList.toggle('rotate')
-  });
-}
-)
-
-whatsApp.addEventListener('click', (e) => {
-  let overlay = document.createElement('div');
-
-  overlay.className = 'popup-overlay';
-
-  document.body.appendChild(overlay);
-
-  let popupBox = document.createElement('div');
-
-  popupBox.className = 'popup-box';
-
-  document.body.appendChild(popupBox)
-
-  let whatsNumber = document.createElement("h3");
-
-  let numberText = document.createTextNode("+201008202048");
-
-  whatsNumber.appendChild(numberText);
-
-  popupBox.appendChild(whatsNumber);
-
-  let closeButton = document.createElement("span");
-
-  let closeButtonText = document.createTextNode("X");
-
-  closeButton.appendChild(closeButtonText);
-
-  popupBox.appendChild(closeButton);
-
-  closeButton.className = 'close-botton';
-})
+// center.addEventListener('click', () => {
+//   social.forEach((e) => {
+//     e.classList.toggle('rotate')
+//   });
+// }
+// )
 
 document.addEventListener('click', function (e) {
   if (e.target.className == 'close-botton') {
@@ -98,13 +64,15 @@ window.onscroll = function () {
 scrollUpBtn.onclick = function () {
   window.scrollTo(0, 0);
 };
-
+let body = document.querySelector("body")
 
 
 window.addEventListener("load", () => {
   setTimeout(() => {
+    body.style.overflow = "auto";
     loader.classList.add("noloader");
-  }, 3000)
+
+  }, 1500)
 })
 
 
